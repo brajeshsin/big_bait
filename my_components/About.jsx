f60b5af501651b090fcbb8796c7d4cea4748140d
brajeshsin <brajeshsingh2911@gmail.com>
2025-06-12 20:50:03 +0530

    Initial commit

---
+++ my_components/About.jsx
@@ -0,0 +1,10 @@
+export default function () {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-10 font-dela text-[75px] text-green">
+        WHAT IS THIS ABOUT?
+      </h1>
+      <div>sdm</div>
+    </div>
+  );
+}
---
+++ my_components/MovingCrads.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, FreeMode } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/autoplay";
+
+const logos = ["Gate.io", "Hello", "Hello", "Hello", "Hello", "Hello", "Hello"];
+
+export default function LogoSwiper() {
+  return (
+    <div className="container max-w-screen-xl mx-auto py-8 px-4">
+      <div className="  flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+        <Swiper
+          modules={[Autoplay, FreeMode]}
+          speed={10000}
+          loop={true}
+          freeMode={true}
+          autoplay={{ delay: 0, disableOnInteraction: false }}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 5 },
+          }}
+        >
+          {logos.map((src, index) => (
+            <SwiperSlide key={index}>
+              <div>
+                <button
+                  className="bg-cream px-8 py-4 border-4 border-black rounded-full cursor-pointer hover:bg-green"
+                  style={{ boxShadow: "2px 1px #000000" }}
+                >
+                  {src}
+                </button>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
