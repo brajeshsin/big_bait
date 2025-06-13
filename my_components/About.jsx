67232b26f23884bd2e61c0b5f986b8c00bcd433f
brajeshsin <brajeshsingh2911@gmail.com>
2025-06-13 11:33:17 +0530

    Fix

--- my_components/About.jsx
+++ my_components/About.jsx
@@ -1,10 +1,23 @@
 export default function () {
   return (
-    <div className="container">
+    <div className="container p-4">
       <h1 className="text-center mt-10 font-dela text-[75px] text-green">
         WHAT IS THIS ABOUT?
       </h1>
-      <div>sdm</div>
+      <div className="bg-amber-500 flex justify-center">
+        <div className=" w-70 h-100 bg-cream flex   p-8 font-dela text-[25px]">
+          <h1 className="text-start">
+            NOTHING IS <br /> IMPOSSIBLE <br /> WITH MEMECOIN
+          </h1>
+        </div>
+        <div className=" w-40 h-40 bg-red-500">
+          <h1>ijfwfwoefvnn</h1>
+        </div>
+
+        <div className=" w-40 h-40 bg-green-600">
+          <h1>Sidvdfzkbn</h1>
+        </div>
+      </div>
     </div>
   );
 }
