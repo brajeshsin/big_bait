296b19946e4b69f7a6d412e5bd4ab5b7b8f2785f
mfaizk <faiz-react@tarality.com>
2025-06-13 12:07:18 +0530

    Meme coin section and global font added

--- common_components/home/MemCoin.jsx
+++ common_components/home/MemCoin.jsx
@@ -2,20 +2,41 @@ import React from "react";
 
 const MemCoin = () => {
   return (
-    <div className="container flex items-center justify-center flex-col">
-      <p className="text-customgreen text-[12rem] font-dela">MEMECOIN</p>
-      <div className="flex justify-around w-full">
+    <div className="container flex items-center justify-center flex-col py-20">
+      <p className="text-customgreen text-4xl lg:text-9xl 2xl:text-[12rem] font-dela">
+        MEMECOIN
+      </p>
+      <div className="w-full my-12 grid grid-cols-12">
         {textItems?.map((item, idx) => {
           return (
-            <p
-              className="text-white font-bricolage hover:text-customgreen cursor-pointer"
+            <div
+              className="col-span-6 lg:col-span-2 flex items-center justify-center"
               key={idx}
             >
-              {item?.label}
-            </p>
+              <p className=" text-white font-bricolage hover:text-customgreen cursor-pointer text-xl hover:scale-110 transition-all duration-200 delay-200 font-semibold ">
+                {item?.label}
+              </p>
+            </div>
           );
         })}
       </div>
+      <button
+        style={{
+          borderColor: "rgb(0, 0, 0)",
+          boxShadow: "rgb(0, 0, 0) 2px 4px 0px 0px",
+          backgroundColor: "rgb(252, 250, 194)",
+          borderBottomWidth: "4px",
+          borderRightWidth: "4px",
+          borderTopWidth: "4px",
+          borderStyle: "solid",
+        }}
+        className="min-w-fit px-14 py-4 rounded-full font-bricolage font-semibold text-4xl transition-all delay-100 duration-300 hover:scale-110 cursor-pointer"
+      >
+        Buy Template
+      </button>
+      <p className="font-bricolage text-white mt-10">
+        → Cryptocurrency Trading Risk Policy
+      </p>
     </div>
   );
 };
