5ca27fe709eed384e1f75b905df03d8f579d6167
brajeshsin <brajeshsingh2911@gmail.com>
2025-06-20 14:47:00 +0530

    Moving infinite responsive fix

--- common_components/home/MovingCrads.jsx
+++ common_components/home/MovingCrads.jsx
@@ -17,8 +17,8 @@ const logos = [
 
 export default function LogoSwiper() {
   return (
-    <div className="container max-w-screen-xl mx-auto py-8 px-4">
-      <div className="  flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+    <div className="container max-w-screen-xl mx-auto py-8 px-4 ">
+      <div className="  flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)] ">
         <Swiper
           modules={[Autoplay, FreeMode]}
           speed={10000}
@@ -29,12 +29,13 @@ export default function LogoSwiper() {
           breakpoints={{
             640: { slidesPerView: 2 },
             768: { slidesPerView: 3 },
-            1024: { slidesPerView: 5 },
+            1024: { slidesPerView: 3 },
+            1440: { slidesPerView: 4 },
           }}
         >
           {logos.map((src, index) => (
             <SwiperSlide key={index}>
-              <div>
+              <div className=" flex justify-center items-center gap-40">
                 <button
                   className="bg-cream px-10 py-5 border-4 border-black rounded-full cursor-pointer hover:bg-customgreen text-black font-bold"
                   style={{ boxShadow: "2px 1px #000000" }}
